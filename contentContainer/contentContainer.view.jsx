264c40e27555f6b1db0fd60688c240f5fad5872d
Alfredo Peres <alfredo@emana.digital>
2019-08-19 11:24:24 -0300

    Migração para NPM e CLSX

--- contentContainer/contentContainer.view.jsx
+++ contentContainer/contentContainer.view.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './contentContainer.style.scss';
-import objstr from 'obj-str';
+import clsx from 'clsx';
 
-const ContentContainer = ({ children, classNameObj }) => (
-	<div className={objstr({ 'content-container': true, ...classNameObj })}>
+const ContentContainer = ({ children, className }) => (
+	<div className={clsx(['content-container', className])}>
 		<div className="content">{children}</div>
 	</div>
 );
 
 ContentContainer.propTypes = {
 	children: PropTypes.node,
-	classNameObj: PropTypes.object
+	className: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
 };
 
 ContentContainer.defaultProps = {
 	children: null,
-	classNameObj: undefined
+	className: undefined
 };
 
 export default ContentContainer;
