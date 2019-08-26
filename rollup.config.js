25bf069ac555c21e6019bd758211c9f2d57a9930
Alfredo Peres <alfredo@emana.digital>
2019-08-26 11:46:12 -0300

    Correções na build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
 import babel from 'rollup-plugin-babel';
+import del from 'rollup-plugin-delete';
 
+// import alias from 'rollup-plugin-alias';
 import pkg from './package.json';
 
 const extensions = ['.mjs', '.js', '.json', '.node', '.jsx', '.ts', '.tsx'];
@@ -11,13 +14,22 @@ export default [
 		input: 'src/index.ts',
 		external: Object.keys(pkg.peerDependencies || {}),
 		plugins: [
+			del({ targets: 'dist/*' }),
+			// alias({
+			// 	resolve: extensions,
+			// 	entries: [{ find: 'utils', replacement: './src/utils' }]
+			// }),
 			resolve({ extensions }),
 			commonjs(),
+			typescript(),
 			babel({ extensions, include: ['src/**/*'] })
 		],
 		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'esm' }
-		]
+			{ file: pkg.main, format: 'cjs', name: pkg.name, sourcemap: true },
+			{ file: pkg.module, format: 'esm', name: pkg.name, sourcemap: true }
+		],
+		watch: {
+			clearScreen: false
+		}
 	}
 ];
