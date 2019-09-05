e4ad75ca93162dbc36dabe16e35c0ee8a820ff60
Alfredo Peres <alfredo@emana.digital>
2019-09-05 11:44:22 -0300

    Correção de Build que não gerava declarações corretas
    O arquivo index.d.ts estava sendo exportado com caminhos errados nos imports

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import ttypescript from 'ttypescript';
 import babel from 'rollup-plugin-babel';
 import del from 'rollup-plugin-delete';
 
-// import alias from 'rollup-plugin-alias';
 import pkg from './package.json';
 
 const extensions = ['.mjs', '.js', '.json', '.node', '.jsx', '.ts', '.tsx'];
@@ -16,13 +16,11 @@ export default [
 		cache: true,
 		plugins: [
 			del({ targets: 'dist/*' }),
-			// alias({
-			// 	resolve: extensions,
-			// 	entries: [{ find: 'utils', replacement: './src/utils' }]
-			// }),
 			resolve({ extensions }),
 			commonjs(),
-			typescript(),
+			typescript({
+				typescript: ttypescript
+			}),
 			babel({ extensions, include: ['src/**/*'] })
 		],
 		output: [
