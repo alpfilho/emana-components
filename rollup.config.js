c092a8497fe6da8d6f3f983e63e20426559e8271
Alfredo Peres <alfredo@emana.digital>
2020-03-28 11:14:56 -0300

    Migração da build para rollup

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import ttypescript from 'ttypescript';
+import babel from 'rollup-plugin-babel';
+import del from 'rollup-plugin-delete';
+
+import pkg from './package.json';
+
+const extensions = ['.mjs', '.js', '.json', '.node', '.jsx', '.ts', '.tsx'];
+
+export default [
+	{
+		input: 'src/index.ts',
+		external: Object.keys(pkg.peerDependencies || {}),
+		cache: true,
+		plugins: [
+			del({ targets: 'dist/*' }),
+			resolve({ extensions }),
+			commonjs(),
+			typescript({
+				/* Typescript com suporte a transforms */
+				typescript: ttypescript
+			}),
+			babel({ extensions, include: ['src/**/*'] })
+		],
+		output: { file: pkg.module, format: 'esm', name: pkg.name, sourcemap: true },
+		watch: {
+			clearScreen: false
+		}
+	}
+];
